d6f0824412f3c34d251fffcbd7a277a13cc854aa
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-06-22 21:03:02 +0200

    completed exercise 3.3

--- phonebook-backend/src/index.js
+++ phonebook-backend/src/index.js
@@ -48,5 +48,14 @@ app.get("/api/info", (req, res) => {
     res.send(resText);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = phoneBook.find(person => person.id === id);
+  if(person)
+    res.json(person)
+  else
+    res.status(404).end();
+})
+
 const PORT = 3001;
 app.listen(PORT, () => `Server listening on port ${PORT}`);
