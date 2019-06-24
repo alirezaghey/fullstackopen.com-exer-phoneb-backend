3f6c1e08b7a9aeab25614aa33fb3996d33c498df
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-06-24 14:45:31 +0200

    added frontend build

---
+++ phonebook-backend/build/precache-manifest.6e06c73c33cd86a66690ef1eb18495c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ff6974158d679af36a0242c5de4288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c16c465ae460ecfb652f",
+    "url": "/static/css/main.742ebdaa.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "c16c465ae460ecfb652f",
+    "url": "/static/js/main.53d97e23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
