25cad0cf3724e42b3c7750dbc6dfed8e641ec5ed
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-07-27 16:11:24 +0200

    completed ex. # 3.15

--- phonebook-backend/src/index.js
+++ phonebook-backend/src/index.js
@@ -17,47 +17,59 @@ app.use(express.static('build'));
 
 const url = process.env.MONGODB_URI;
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   personDB.getAllPersons(url).then(persons => {
-    res.json(persons.map(per => {return {id: per._id, name: per.name, number: per.number}}));
+    res.json(
+      persons.map(per => {
+        return { id: per._id, name: per.name, number: per.number };
+      })
+    );
   });
 });
 
-app.get("/api/info", (req, res) => {
-    personDB.getPersonsCount(url).then(count => {
-      const resText = `Phonebook has info for ${count} people!<br/>${Date(Date.now).toString()}`
-      res.send(resText);
-    })
+app.get('/api/info', (req, res) => {
+  personDB.getPersonsCount(url).then(count => {
+    const resText = `Phonebook has info for ${count} people!<br/>${Date(
+      Date.now
+    ).toString()}`;
+    res.send(resText);
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id;
   personDB.getPersonByID(url, id).then(person => {
-    if(person)
-      res.json(person)
-    else
-      res.status(404).end();
+    if (person) res.json(person);
+    else res.status(404).end();
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  phoneBook = phoneBook.filter(person => person.id !== id)
-  res.status(204).end();
-})
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  personDB
+    .deletePerson(url, id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => console.log(error));
+});
 
-app.post("/api/persons", (req, res) => {
+app.post('/api/persons', (req, res) => {
   const person = req.body;
 
-  if (person.name === "")
-    return res.status(400).json({error: "name missing!"});
-  if (person.number === "")
-    return res.status(400).json({error: "number missing!"});
-  personDB.addPerson(url, person.name, person.number).then(person => res.json(person));
-})
+  if (person.name === '')
+    return res.status(400).json({ error: 'name missing!' });
+  if (person.number === '')
+    return res.status(400).json({ error: 'number missing!' });
+  personDB
+    .addPerson(url, person.name, person.number)
+    .then(person => res.json(person));
+});
 
-app.put("/api/persons/:id", (req, res) => {
-  personDB.updatePerson(url, req.body.id, req.body.number).then(person => res.json(person));
+app.put('/api/persons/:id', (req, res) => {
+  personDB
+    .updatePerson(url, req.body.id, req.body.number)
+    .then(person => res.json(person));
 });
 
 const PORT = process.env.PORT || 3001;
--- phonebook-backend/src/personDB.js
+++ phonebook-backend/src/personDB.js
@@ -1,61 +1,72 @@
 const mongoose = require('mongoose');
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 });
-
 
 const Person = mongoose.model('Person', personSchema);
 
-const getAllPersons = (url) => {
-    mongoose.connect(url, {useNewUrlParser: true});
-    return Person.find({}).then( res => {
-        mongoose.connection.close();
-        return res;
-    });
-}
+const getAllPersons = url => {
+  mongoose.connect(url, { useNewUrlParser: true });
+  return Person.find({}).then(res => {
+    mongoose.connection.close();
+    return res;
+  });
+};
 
 const getPersonByID = (url, id) => {
-    mongoose.connect(url, {useNewUrlParser: true});
-    return Person.findById(id).then(res => {
-        mongoose.connection.close();
-        return res;
-    });
-}
+  mongoose.connect(url, { useNewUrlParser: true });
+  return Person.findById(id).then(res => {
+    mongoose.connection.close();
+    return res;
+  });
+};
 
-const getPersonsCount = (url) => {
-    mongoose.connect(url, {useNewUrlParser: true});
-    return Person.estimatedDocumentCount({}).then( res => {
-        mongoose.connection.close();
-        return res;
-    })
-}
+const getPersonsCount = url => {
+  mongoose.connect(url, { useNewUrlParser: true });
+  return Person.estimatedDocumentCount({}).then(res => {
+    mongoose.connection.close();
+    return res;
+  });
+};
 
 const addPerson = (url, name, number) => {
-    const person = new Person({
-        name: name,
-        number: number
-    });
-    mongoose.connect(url, {useNewUrlParser: true});
-    return person.save().then(res => {
-        mongoose.connection.close();
-        return res;
-    });
+  const person = new Person({
+    name: name,
+    number: number
+  });
+  mongoose.connect(url, { useNewUrlParser: true });
+  return person.save().then(res => {
+    mongoose.connection.close();
+    return res;
+  });
 };
 
 const updatePerson = (url, id, number) => {
-    mongoose.connect(url, {useNewUrlParser: true, useFindAndModify: false});
-    return Person.findByIdAndUpdate(id, {number: number}, {new: true}).then(res => {
-        mongoose.connection.close();
-        return res;
-    })
-}
+  mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false });
+  return Person.findByIdAndUpdate(id, { number: number }, { new: true }).then(
+    res => {
+      mongoose.connection.close();
+      return res;
+    }
+  );
+};
+
+const deletePerson = (url, id) => {
+  mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false });
+  return Person.findByIdAndRemove(id).then(res => {
+    mongoose.connection.close();
+    console.log(res);
+    return res;
+  });
+};
 
 module.exports = {
-    getAllPersons: getAllPersons,
-    addPerson: addPerson,
-    updatePerson: updatePerson,
-    getPersonsCount: getPersonsCount,
-    getPersonByID: getPersonByID
-}
+  getAllPersons: getAllPersons,
+  addPerson: addPerson,
+  updatePerson: updatePerson,
+  getPersonsCount: getPersonsCount,
+  getPersonByID: getPersonByID,
+  deletePerson: deletePerson
+};
